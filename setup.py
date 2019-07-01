b6de821824447ea46710f9a4e345cfabfa3005c0
Carl Lemaire <c.lemaire@live.ca>
2019-07-01 11:59:22 -0400

    Make package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stylegan',
+      version='0.1',
+      description='StyleGAN Generator',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['stylegan'],
+      zip_safe=False)
---
+++ stylegan/generate.py
@@ -0,0 +1,78 @@
+import time
+
+import torch
+from torchvision import utils
+
+from model import StyledGenerator
+
+
+generate_mixing = False
+
+device = 'cpu'
+generator = StyledGenerator(512).to(device)
+generator.load_state_dict(torch.load('checkpoint/style-gan-256-140k.model', map_location=device))
+
+mean_style = None
+
+step = 6
+
+shape = 4 * 2 ** step
+
+mean_steps = 1
+for i in range(mean_steps):
+    style = generator.mean_style(torch.randn(10, 512).to(device))
+
+    if mean_style is None:
+        mean_style = style
+
+    else:
+        mean_style += style
+
+mean_style /= mean_steps
+
+n_samples = 20
+image = generator(
+    torch.randn(n_samples, 512).to(device),
+    step=step,
+    alpha=1,
+    mean_style=mean_style,
+    style_weight=-3,
+)
+
+utils.save_image(image, 'sample_{}.png'.format(time.time()), nrow=10, normalize=True, range=(-1, 1))
+
+if generate_mixing:
+    for j in range(20):
+        source_code = torch.randn(9, 512).to(device)
+        target_code = torch.randn(5, 512).to(device)
+    
+        images = [torch.ones(1, 3, shape, shape).to(device) * -1]
+    
+        source_image = generator(
+            source_code, step=step, alpha=1, mean_style=mean_style, style_weight=0.7
+        )
+        target_image = generator(
+            target_code, step=step, alpha=1, mean_style=mean_style, style_weight=0.7
+        )
+    
+        images.append(source_image)
+    
+        for i in range(5):
+            image = generator(
+                [target_code[i].unsqueeze(0).repeat(9, 1), source_code],
+                step=step,
+                alpha=1,
+                mean_style=mean_style,
+                style_weight=0.7,
+                mixing_range=(0, 1),
+            )
+            images.append(target_image[i].unsqueeze(0))
+            images.append(image)
+    
+        # print([i.shape for i in images])
+    
+        images = torch.cat(images, 0)
+    
+        utils.save_image(
+            images, f'sample_mixing_{j}.png', nrow=10, normalize=True, range=(-1, 1)
+        )
