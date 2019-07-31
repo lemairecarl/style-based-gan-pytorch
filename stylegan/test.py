dbf9037a46cf4a42669901b63ba5a093d9d71958
Carl Lemaire <c.lemaire@live.ca>
2019-07-30 20:47:33 -0400

    Test GeneratorClient

--- stylegan/test.py
+++ stylegan/test.py
@@ -1,9 +1,10 @@
 import stylegan
+import stylegan.client
 import torch
 
 from stylegan import save_image
 
-g = stylegan.GeneratorClient()
+g = stylegan.client.GeneratorClient()
 
 p = torch.randn(512)
 save_image(g.generate(p, True))
